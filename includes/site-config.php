3e7ec1ef6bc8480e00b70a67ed7b2deb41b33fd7
subhransusekhar <subhransu.sm@gmail.com>
2013-09-07 11:23:13 +0530

    ezfoot config

--- includes/site-config.php
+++ includes/site-config.php
@@ -1,6 +1,6 @@
 <?php 
 /* site config*/ 
-define( 'SITE_NAME', 'Zurl.in');
+define( 'SITE_NAME', 'ezfoot');
 define( 'SITE_SLOGAN', 'Shorten your URL');
 define( 'COPY_RIGHT', 'Pietas Labs');
 define( 'COPY_RIGHT_URL', 'http://www.pietaslabs.com');
@@ -10,18 +10,18 @@ if($_SERVER['SERVER_NAME'] == 'localhost' ) {
 	define( 'ROOT_DIR', 'zurl' );
 	define( 'SITE_URL', 'http://localhost/'.ROOT_DIR.'/' );
 	define( 'SITE_URL_LINK', 'http://localhost/'.ROOT_DIR .'/');
-} else if($_SERVER['SERVER_NAME'] == 'dev.zurl.in' ) {
+} else if($_SERVER['SERVER_NAME'] == 'dev.ezfoot' ) {
 	define( 'ROOT_DIR', '' );
-	define( 'SITE_URL', 'http://dev.zurl.in/'.ROOT_DIR );
-	define( 'SITE_URL_LINK', 'http://dev.zurl.in/'.ROOT_DIR );
-} else if($_SERVER['SERVER_NAME'] == 'www.zurl.in' ) {
+	define( 'SITE_URL', 'http://dev.ezfoot/'.ROOT_DIR );
+	define( 'SITE_URL_LINK', 'http://dev.ezfoot/'.ROOT_DIR );
+} else if($_SERVER['SERVER_NAME'] == 'www.ezfoot' ) {
 	define( 'ROOT_DIR', '' );
-	define( 'SITE_URL', 'http://www.zurl.in/'.ROOT_DIR );
-	define( 'SITE_URL_LINK', 'http://www.zurl.in/'.ROOT_DIR );
+	define( 'SITE_URL', 'http://www.ezfoot/'.ROOT_DIR );
+	define( 'SITE_URL_LINK', 'http://www.ezfoot/'.ROOT_DIR );
 } else {
 	define( 'ROOT_DIR', '' );
-	define( 'SITE_URL', 'http://zurl.in/'.ROOT_DIR );
-	define( 'SITE_URL_LINK', 'http://zurl.in/'.ROOT_DIR );
+	define( 'SITE_URL', 'http://ezfoot/'.ROOT_DIR );
+	define( 'SITE_URL_LINK', 'http://ezfoot/'.ROOT_DIR );
 }
 
 define( 'SITE_MAINTENANCE', 'maintenance' );
